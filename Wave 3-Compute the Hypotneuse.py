ac99fe7ddc13d684bc73157c8d8a681c9efb8fdd
Leoxyh <58529404+Leoxyh@users.noreply.github.com>
2020-05-15 23:31:40 -0400

    Add files via upload

---
+++ Wave 3-Compute the Hypotneuse.py
@@ -0,0 +1,11 @@
+from math import sqrt
+
+
+def num(answer, d):
+    answer = sqrt(first_side*first_side+second_side*second_side)
+    print(answer)
+
+
+first_side = int(input("Enter your first side: "))
+second_side = int(input("Enter your second side: "))
+num(first_side, second_side)
